186d148956544ff76dec523d126a22817811906d
lidahao <alwaysloseall@gmail.com>
2022-05-17 18:07:55 +0800

    fixStyleSheet 和 setPX 支持baseWidth参数

--- packages/utils/src/fixStyleSheet.ts
+++ packages/utils/src/fixStyleSheet.ts
@@ -5,9 +5,9 @@ const rules = ['opacity', 'flex']
 const fixRule = (key: string, value: any) => {
   return typeof value === 'number' && !rules.includes(key)
 }
-const setPX = (value: number) => {
+const setPX = (value: number, baseWidth: number = 750) => {
   const { width } = Dimensions.get('window')
-  return (value * width) / 750
+  return (value * width) / baseWidth
 }
 
 interface objectMap {
@@ -16,14 +16,14 @@ interface objectMap {
 
 function fixStyleSheet<
   T extends StyleSheet.NamedStyles<T> | StyleSheet.NamedStyles<any>
->(styles: T | StyleSheet.NamedStyles<T>): T {
+>(styles: T | StyleSheet.NamedStyles<T>, options?: { baseWidth?: number }): T {
   let newStyles: any = {}
   Object.keys(styles).forEach(key => {
     let newSubStyle: any = {}
     let value = (styles as objectMap)[key]
     Object.keys(value).forEach(subKey => {
       if (fixRule(subKey, value[subKey])) {
-        newSubStyle[subKey] = setPX(value[subKey])
+        newSubStyle[subKey] = setPX(value[subKey], options?.baseWidth)
       } else if (typeof value[subKey] === 'object') {
         if (value[subKey]['pixelRatio']) {
           newSubStyle[subKey] = value[subKey]['value'] / PixelRatio.get()
